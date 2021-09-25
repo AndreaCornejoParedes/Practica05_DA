a9ea771578d3bf3e2f0d4a1899e417eb66e9de96
Mirvane <mirvane32xa@gmail.com>
2021-09-25 12:22:12 -0500

    EjercicioPropuesto

---
+++ EjercicioPropuesto_1/js/script.js
@@ -0,0 +1,18 @@
+﻿function guardar(n,p) {
+    let Nom = [];
+    let Pues = [];
+
+    let nomLista = Nom.push(n);
+    let puesLista = Nom.push(p);
+    
+    console.log(Nom.length)
+    console.log(Pues.length)
+}
+
+function calcular(h1) {
+    var hora_total = h1 * 20;
+    return (hora_total * 5.8125);
+}
+
+var sueldo_mensual = calcular(horas);
+var registro = guardar(nombre,puesto);
---
+++ EjercicioPropuesto_2/js/script.js
@@ -0,0 +1,15 @@
+function mostrarSoluciones() {
+    let nt
+    let obj
+    nt = document.createTextNode('Nodo 1')
+    obj = document.getElementById('opcion1')
+    obj.appendChild(nt)
+    nt = document.createTextNode('Nodo 2')
+    obj = document.getElementById('opcion2')
+    obj.appendChild(nt)
+    nt = document.createTextNode('Nodo 3')
+    obj = document.getElementById('opcion3')
+    obj.appendChild(nt)
+}
+
+
